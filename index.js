5d5df3d918a0a8d825f0dbb20e10468f78b37672
TechForEverybody <Pankajsinghat95@gmail.com>
2023-10-28 14:36:14 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const ip = require('ip');
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log("Server is running");
+    res.send('Hello from Server');
+})
+
+app.get('/forward', (req, res) => {
+    console.log("forward");
+    res.send("forward");
+})
+
+app.get('/backward', (req, res) => {
+    console.log("backward");
+    res.send("backward");
+})
+
+app.get('/left', (req, res) => {
+    console.log("left");
+    res.send("left");
+})
+
+app.get('/right', (req, res) => {
+    console.log("right");
+    res.send("right");
+})
+
+app.get('/stop', (req, res) => {
+    console.log("stop");
+    res.send("stop");
+})
+
+app.get("/leftTurn", (req, res) => {
+    console.log("leftTurn");
+    res.send("leftTurn");
+})
+
+app.get("/rightTurn", (req, res) => {
+    console.log("rightTurn");
+    res.send("rightTurn");
+})
+
+
+app.listen(80, () => {
+    console.log(`Server is running http://${ip.address()}`);
+})
